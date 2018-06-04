ced736eae2e447ac77ed7e020f3da82564ba5a01
Grenville Sequeira <grenville.sequeira@gmail.com>
2018-06-03 23:53:21 -0400

    Removed Spring dependency from DAOUserService.

--- src/main/java/us/acton/grenville/restdemo/service/DAOUserService.java
+++ src/main/java/us/acton/grenville/restdemo/service/DAOUserService.java
@@ -1,22 +1,17 @@
 package us.acton.grenville.restdemo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import us.acton.grenville.restdemo.dao.UserDao;
 import us.acton.grenville.restdemo.entity.User;
 import us.acton.grenville.restdemo.service.user.UserDTO.ApiDTOBuilder;
 import us.acton.grenville.restdemo.service.user.UserDTO.UserDTO;
 
-@Component
 public class DAOUserService implements UserService {
 
-    @Autowired
     final private UserDao userDao;
 
     public DAOUserService(UserDao userDao) {
         this.userDao = userDao;
     }
-
 
     @Override
     public UserDTO getUserByUsername(String username) {
